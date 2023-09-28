e195a002eaf87e7b6524dcf51e1f04f5ba645df4
Kevin Hanard <kevin.hanard@gmail.com>
2023-09-28 14:01:15 +1000

    feat: flip card next

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useSpring, a } from '@react-spring/web';
 import cards from './data';
 import './index.css';
@@ -6,12 +6,28 @@ import Memory from './components/memory';
 
 function App() {
   const [card, setCard] = useState(cards);
+  const [currcards, setCurrcards] = useState([]);
+  const [cardok, setCardok] = useState({});
+
   // const [card, setCard] = useState(() => shuffle(cards.concat(cards)));
   const [flip, setFlip] = useState(false);
+  const timeout = useRef(null);
 
   return (
     <div className='App'>
-      <Memory card={card} cards={cards} flip={flip} setFlip={setFlip} />
+      <div className='banner'>
+        <div className='score'>Score</div>
+        <div className='score'>Score</div>
+      </div>
+      <Memory
+        card={card}
+        currcards={currcards}
+        setCurrcards={setCurrcards}
+        cards={cards}
+        flip={flip}
+        setFlip={setFlip}
+        timeout={timeout}
+      />
     </div>
   );
 }
--- src/components/memory.js
+++ src/components/memory.js
@@ -1,11 +1,34 @@
 import { useState } from 'react';
-const Memory = ({ card, cards, flip, setFlip }) => {
-  const [save, setSave] = useState(null);
-  const handleClick = (id) => {
-    setFlip(!flip);
-    const updatedid = id;
-    setSave(updatedid);
+const Memory = ({
+  card,
+  cards,
+  flip,
+  setFlip,
+  setCurrcards,
+  currcards,
+  timeout,
+}) => {
+  // const [save, setSave] = useState(null);
+  const handleCardClick = (index) => {
+    if (currcards.length === 1) {
+      setCurrcards((prev) => [...prev, index]);
+    } else {
+      clearTimeout(timeout.current);
+      setCurrcards([index]);
+    }
   };
+  console.log('currcard', currcards);
+  const checkIsFlipped = (index) => {
+    return currcards.includes(index);
+  };
+  const handleClick = (index) => {
+    !checkIsFlipped(index) && handleCardClick(index);
+    console.log(index);
+    // setFlip(!flip);
+    // const updatedid = index;
+    // setSave(updatedid);
+  };
+
   console.log(card);
   return (
     <div className='memory-game'>
@@ -14,7 +37,8 @@ const Memory = ({ card, cards, flip, setFlip }) => {
         return (
           <div
             key={id}
-            className={`card-game ${flip && save == id ? 'flip' : ''}`}
+            className={`card-game ${checkIsFlipped(id) ? 'flip' : ''}`}
+            // className={`card-game ${flip && save == id ? 'flip' : ''}`}
             onClick={() => handleClick(id)}
           >
             {/* <p>{name}</p> */}
